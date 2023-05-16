5f24d8a4541e851455a0a52bc6a52a0f8edf085c
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-16 23:44:38 +0600

    update register

--- src/Components/Layout/Pages/Post Job/PostJob.jsx
+++ src/Components/Layout/Pages/Post Job/PostJob.jsx
@@ -1,124 +1,186 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import CreatableSelect from "react-select/creatable";
 
 const PostJob = () => {
-  const [selectedOption, setSelectedOption] = useState(null);
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => {
-    fetch("http://localhost:4000/postJobs", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-    data.skills = selectedOption;
-    console.log(data);
+  const handlePostJobs = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const title = form.title.value;
+    const salary = form.salary.value;
+    const vacancy = form.vacancy.value;
+    const category = form.category.value;
+    const statues = form.statues.value;
+    const deadline = form.deadline.value;
+    const postedBy = from.postedBy.value;
+    const description = form.description.value;
+    const options = from.options.value;
+    const image = form.image.value;
+
+    const posted = {
+      title,
+      salary,
+      vacancy,
+
+      category,
+      statues,
+      deadline,
+      postedBy,
+      description,
+      image,
+      options,
+    };
+    console.log(newChocolate);
   };
 
-  const options = [
-    { value: "JavaScript", label: "JavaScript" },
-    { value: "C++", label: "C++" },
-    { value: "HTML", label: "HTML" },
-    { value: "CSS", label: "CSS" },
-    { value: "React", label: "React" },
-    { value: "Node", label: "Node" },
-    { value: "MongoDB", label: "MongoDB" },
-    { value: "Redux", label: "Redux" },
-  ];
-
   return (
-    <div className="  ">
-      <div className=" grid grid-cols-1 lg:grid-cols-2 ">
-        <div className="mx-auto">
-          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col p-4">
-            {errors.exampleRequired && <span>This field is required</span>}
-            <input
-              className="text-input mb-4"
-              {...register("title")}
-              placeholder="title"
-              defaultValue="Web developer"
-            />
+    <div className="bg-orange-100 mt-5 p-24">
+      <h2 className="text-3xl font-extrabold">Upload Your Jobs</h2>
+      <form onSubmit={handlePostJobs}>
+        {/* form name and quantity row */}
+        <div className=" mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text text-lg font-medium">Post Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="title"
+                placeholder="Jobs Title"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2 mt-5  ">
+            <label className="label">
+              <span className="label-text text-lg font-medium">Salary</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="salary"
+                placeholder=" Enter Salary"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2 mt-5  ">
+            <label className="label">
+              <span className="label-text text-lg font-medium">Vacancy</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="vacancy"
+                placeholder=" Enter SNumber of vacancy"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
 
+        <div className="md:flex mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text text-lg font-medium">Category</span>
+            </label>
+            <div className="input-group">
+              <select className="select w-full select-bordered" name="category">
+                <option>Engineering</option>
+                <option>Editor</option>
+                <option>Writer</option>
+                <option>Developer</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="md:flex mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text text-lg font-medium">Status</span>
+            </label>
+            <div className="input-group">
+              <select className="select w-full select-bordered" name="statues">
+                <option value="remote">Remote</option>
+                <option value="offline">Offline</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        {/* form Photo url row */}
+        <div className="mb-8">
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text text-lg font-medium">Photo URL</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="image"
+                placeholder="Photo URL"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        <div className="form-control md:w-1/2 mt-5  ">
+          <label className="label">
+            <span className="label-text text-lg font-medium">Dead Line</span>
+          </label>
+          <label className="input-group">
             <input
-              className="text-input mb-4"
-              {...register("salary", { required: true })}
-              placeholder="salary"
-              defaultValue="30k"
-            />
-            <input
-              className="text-input mb-4"
-              {...register("vacancy", { required: true })}
-              placeholder="vacancy"
-              type="number"
-            />
-            <select className="text-input mb-4" {...register("category")}>
-              <option value="Engineering">engineering</option>
-              <option value="Editor">Editor</option>
-              <option value="writer">Writer</option>
-              <option value="Developer">Developer</option>
-            </select>
-            <select className="text-input mb-4" {...register("status")}>
-              <option value="remote">Remote</option>
-              <option value="offline">Offline</option>
-            </select>
-            <input
-              className="text-input mb-4"
-              {...register("image")}
-              placeholder="image link"
-              type="url"
-              defaultValue="https://images.pexels.com/photos/2528118/pexels-photo-2528118.jpeg?auto=compress&cs=tinysrgb&w=600"
-            />
-            <input
-              className="text-input mb-4"
-              {...register("deadline")}
-              placeholder="deadline"
               type="date"
+              name="deadline"
+              placeholder=" Enter your deadline"
+              className="input input-bordered w-full"
             />
-            <input
-              className="text-input mb-4"
-              {...register("postedBy")}
-              placeholder="your email"
-              type="email"
-            />
-            <CreatableSelect
-              className="w-75"
-              defaultValue={selectedOption}
-              onChange={setSelectedOption}
-              options={options}
-              isMulti
-            />
-            <input
-              className="text-input mb-4"
-              {...register("description")}
-              placeholder="description"
-            />
-            <input
-              className="btn btn-primary mb-4"
-              value="Post Job"
-              type="submit"
-            />
-          </form>
+          </label>
+          <div className="form-control md:w-1/2 mt-5  ">
+            <label className="label">
+              <span className="label-text text-lg font-medium">Email</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="email"
+                name="postedBy"
+                placeholder=" Enter your mail"
+                className="input input-bordered w-full"
+                value={user?.email}
+              />
+            </label>
+          </div>
         </div>
-        <div className=" ">
-          <img
-            className="w-100"
-            src="https://i.ibb.co/rthZ75K/pngtree-job-vacancy-with-join-our-team-recruitment-vector-design-png-image-6419066-removebg-preview.png"
-            alt=""
+        <div className="md:flex mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text text-lg font-medium"></span>
+            </label>
+            <div className="input-group">
+              <select className="select w-full select-bordered" name="options">
+                <option>JavaScript</option>
+                <option>HTML</option>
+                <option>CSS</option>
+                <option>Node</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <label className="input-group">
+          <input
+            type="text"
+            name="description"
+            placeholder=" Enter Description about this job"
+            className="input input-bordered w-full"
           />
-        </div>
-      </div>
+        </label>
+        <input
+          type="submit"
+          value="Post Job"
+          className="btn btn-block bg-orange-800"
+        />
+      </form>
     </div>
   );
 };
